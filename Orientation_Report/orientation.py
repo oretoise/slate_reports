54effeb0d8eb0ac0c3c6adc5f1eae56282e491f9
Maxwell Perkins <mperk1000@gmail.com>
2021-11-02 10:43:34 -0500

    Further cleaning for 4 final columns

--- Orientation_Report/orientation.py
+++ Orientation_Report/orientation.py
@@ -44,16 +44,8 @@ def data_to_df(data_path):
     df = pd.concat(dfs)
     return df
 
-def main():
+def eliminate_columns(df):
 
-    # aggregate the data into a big dataframe
-    df = data_to_df(pathlib.Path(os.getcwd()) / "data")
-
-    
-    df = df.transpose()
-
-    # print(df.index.str.contains("Day", na=False))
-    
     df = df[[not elem for elem in df.index.str.contains("Day", na=False)]]
         
     list = ['MSU', 'digit', 'Digit', 'Date', 'Admit', 'Final', 'Term', 'term', '#', 'ID', 'Total', 'First', 'Last', 'Name', 'Student','student', 'Id', 'Totat', 'Semester', 'Username', 'Date']
@@ -73,13 +65,53 @@ def main():
             continue
         boolean_list.append(False)
 
-    print(df.shape)
-
     df = df[boolean_list]
 
-    print(df.shape)
+    list = ['Final Score', 'Assignments Unposted Final Score', 'Unposted Final Score', 'Assignments Final Points', 'Imported Assignments Final Score','Imported Assignments Final Points', 'Imported Assignments Unposted Final Score', 'ID', 'Assignments Final Score']
     
-    df.to_excel("test.xlsx")
+    return df.drop(labels=list, axis=0).transpose().drop(index=0)
+
+def combine_admit(df):
+    return df[(df.index.str.contains('admit', case=False)) | (df.index.str.contains('term', case=False) | (df.index.str.contains('semester', case=False)))].transpose()
+
+
+def combine_netID(df):
+    return df[(df.index.str.contains('Net')) | (df.index.str.contains('SIS Login') | (df.index.str.contains('Username')))].transpose()
+
+
+def combine_9_digit(df):
+    return df[(df.index.str.contains('9')) | (df.index.str.contains('MSU') | (df.index.str.contains('0') | (df.index.str.contains('SIS User') | (df.index.str.contains('Student ID')))))].transpose()
+
+def combine_scores(df):
+    return df[(df.index.str.contains('Fina')) | (df.index.str.contains('Tota'))].transpose()
+
+def construct_result(df):
+    list = []
+    list.append(combine_scores(df.transpose()))
+    list.append(combine_9_digit(df.transpose()))
+    list.append(combine_netID(df.transpose()))
+    list.append(combine_admit(df.transpose()))
+
+    # TODO: since concattenating data is being separated fix this later
+    return pd.concat(list)
+
+def main():
+
+    # aggregate the data into a big dataframe
+    df = data_to_df(pathlib.Path(os.getcwd()) / "data")    
+
+    # eliminate unwanted columns
+    df = eliminate_columns(df.transpose())
+
+    # condense into 4 columns
+    final_df = construct_result(df)
+
+    
+    ''' for debugging '''
+    # final_df.to_excel("final_df.xlsx")
+    # df.to_excel("df.xlsx")
+    print(final_df.shape)
+    
 
 if __name__ == "__main__":
     # Parse CLI arguments.
