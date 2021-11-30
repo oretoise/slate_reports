ab0199f3e9ef0933c712ab360a6e82131b998f53
Maxwell Perkins <mperk1000@gmail.com>
2021-11-30 09:20:49 -0600

    set dataframe will create new worksheet upon error

--- Google/gsheets.py
+++ Google/gsheets.py
@@ -13,13 +13,17 @@ def set_dataframe(client, df, worksheet):
 
     # Get GSheets document using client.
     gdoc = client.open("Jordan's File")
-
-    # Get specified worksheet.
-    sheet = gdoc.worksheet('title', worksheet)
+    try:
+        # Get specified worksheet.
+        sheet = gdoc.worksheet('title', worksheet)
+       
+    except pygsheets.exceptions.WorksheetNotFound:
+        # Create new worksheet and then get specified worksheet
+        gdoc.add_worksheet(worksheet)
+        sheet = gdoc.worksheet('title', worksheet)
 
     # Set the sheet contents to the provided dataframe.
     sheet.set_dataframe(df,(1,1))
-
     # Sync it.
     sheet.sync()
 
